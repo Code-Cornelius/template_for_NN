33791d1d77c817681161d4802711683d6f6f9683
Catea Bianca <cateabianca@gmail.com>
2021-03-06 20:34:29 +0100

    Add EarlyStopping class with checkpoint

--- src/class.py
+++ src/class.py
@@ -101,7 +101,7 @@ class NNTrainParameters:
     @batch_size.setter
     def batch_size(self, new_batch_size):
         if isinstance(new_batch_size, int) and new_batch_size >= 0:
-                self._batch_size = new_batch_size
+            self._batch_size = new_batch_size
         else:
             raise TypeError(f'Argument is not an {str()}.')
 
@@ -112,7 +112,7 @@ class NNTrainParameters:
     @learning_rate.setter
     def learning_rate(self, new_learning_rate):
         if isinstance(new_learning_rate, float):
-                self._learning_rate = new_learning_rate
+            self._learning_rate = new_learning_rate
         else:
             raise TypeError(f'Argument is not an {str()}.')
 
@@ -123,7 +123,7 @@ class NNTrainParameters:
     @epochs.setter
     def epochs(self, new_epochs):
         if isinstance(new_epochs, int) and new_epochs >= 0:
-                self._epochs = new_epochs
+            self._epochs = new_epochs
         else:
             raise TypeError(f'Argument is not an {str()}.')
 
@@ -142,3 +142,61 @@ class NNTrainParameters:
     @optimiser.setter
     def optimiser(self, new_optimiser):
         self._optimiser = new_optimiser
+
+
+class EarlyStopping:
+    """
+    Early stops the training if validation loss doesn't improve after a given patience.
+    References:
+            https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
+    """
+
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                return True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+        return False
+
+    def save_checkpoint(self, val_loss, model):
+        """
+        Saves model when validation loss decrease.
+        """
+        if self.verbose:
+            self.trace_func(
+                f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
