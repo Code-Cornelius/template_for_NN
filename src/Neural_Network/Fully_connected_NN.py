4fe6e846f50f263c8e6530077485b59f396ecd31
CoRneLIUs <niels.carioukotlarek@yahoo.com>
2021-04-02 23:12:07 +0200

    Parameter predict_fct. Refactored name layer by _layer.

--- src/Neural_Network/Fully_connected_NN.py
+++ src/Neural_Network/Fully_connected_NN.py
@@ -3,12 +3,15 @@ import torch
 import torch.nn as nn
 
 
+def my_id(out):
+    return out
+
 # the class of NN
 class Fully_connected_NN(nn.Module):
 
     def __init__(self, list_hidden_sizes, hidden_sizes, output_size,
                  list_biases, activation_functions,
-                 dropout=0):
+                 dropout=0, predict_fct = None):
         """
         Constructor for Neural Network
 
@@ -29,38 +32,44 @@ class Fully_connected_NN(nn.Module):
         self.list_biases = list_biases
         self.activation_functions = activation_functions
 
+        #function that from the output returns the prediction. Depends on the problem.
+        if predict_fct is None:
+            self.predict_fct = my_id
+        else:
+            self.predict_fct = predict_fct
+
         # array of fully connected layers
-        self.layers = nn.ModuleList()
+        self._layers = nn.ModuleList()
 
         # initialise the input layer
-        self.layers.append(nn.Linear(self.input_size, self.list_hidden_sizes[0], self.list_biases[0]))
+        self._layers.append(nn.Linear(self.input_size, self.list_hidden_sizes[0], self.list_biases[0]))
 
         # initialise the hidden layers
         for i in range(len(hidden_sizes) - 1):
-            self.layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1], self.list_biases[i + 1]))
+            self._layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1], self.list_biases[i + 1]))
 
         # initialise the output layer
-        self.layers.append(nn.Linear(self.list_hidden_sizes[-1], self.output_size, self.list_biases[-1]))
+        self._layers.append(nn.Linear(self.list_hidden_sizes[-1], self.output_size, self.list_biases[-1]))
 
         # initialise dropout
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
         # pass through the input layer
-        out = self.activation_functions[0](self.layers[0](x))
+        out = self.activation_functions[0](self._layers[0](x))
 
         # pass through the hidden layers
         for layer_index in range(1, len(self.list_hidden_sizes) - 1):
-            out = self.activation_functions[layer_index](self.dropout(self.layers[layer_index](out)))
+            out = self.activation_functions[layer_index](self.dropout(self._layers[layer_index](out)))
 
         # pass through the output layer
-        out = self.layers[-1](out)
+        out = self._layers[-1](out)
         return out
 
     def prediction(self, out):
         """returns the class predicted for each element of the tensor."""
         # gets the class that is max probability
-        return torch.max(out, 1)[1]
+        return self.predict_fct(out)
 
     def init_weights_of_model(self):
         """Initialise weights of the model such that they have a predefined structure"""
