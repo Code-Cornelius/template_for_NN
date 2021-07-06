2b06b77ea4c8ac74a1523fd574d2d6c783a6bb7c
CoRneLIUs <niels.carioukotlarek@yahoo.com>
2021-07-06 17:37:14 +0200

    example hyper param

--- tests/examples_of_tasks/example_hyper_param.py
+++ tests/examples_of_tasks/example_hyper_param.py
@@ -1,23 +1,20 @@
-from priv_lib_util.tools.src.function_dict import parameter_product
-
-from nn_classes.estimator.history.relplot_history import Relplot_history
-from nn_classes.estimator.hyper_parameters.estim_hyper_param import Estim_hyper_param
-from nn_classes.estimator.hyper_parameters.distplot_hyper_param import Distplot_hyper_param
-from src.nn_classes.optim_wrapper import Optim_wrapper
-from priv_lib_plot import APlot
-
-import torch
-from torch import nn
 import numpy as np
+import torch
+from priv_lib_plot import APlot
+from priv_lib_util.tools.src.function_dict import parameter_product
+from torch import nn
+from tqdm import tqdm
 
-from plot.nn_plots import nn_plot_prediction_vs_true, nn_errors_compute_mean
+from nn_classes.estimator.hyper_parameters.distplot_hyper_param import Distplot_hyper_param
+from nn_classes.estimator.hyper_parameters.estim_hyper_param import Estim_hyper_param
+from nn_train.kfold_training import nn_kfold_train
 from src.nn_classes.architecture.fully_connected import factory_parametrised_FC_NN
-from src.nn_train.nntrainparameters import NNTrainParameters
-from src.util_training import set_seeds, pytorch_device_setting
+from src.nn_classes.metric.metric import Metric
+from src.nn_classes.optim_wrapper import Optim_wrapper
 from src.nn_classes.training_stopper.Early_stopper_training import Early_stopper_training
 from src.nn_classes.training_stopper.Early_stopper_validation import Early_stopper_validation
-from nn_train.kfold_training import nn_kfold_train
-from src.nn_classes.metric.metric import Metric
+from src.nn_train.nntrainparameters import NNTrainParameters
+from src.util_training import set_seeds, pytorch_device_setting
 
 
 # Define the exact solution
@@ -27,6 +24,7 @@ def exact_solution(x):
 
 def L4loss(net, xx, yy):
     return torch.norm(net.nn_predict(xx) - yy, 4)
+
 
 L4metric = Metric('L4', L4loss)
 metrics = (L4metric,)
@@ -57,17 +55,17 @@ train_Y = yy[:training_size, :]
 testing_X = xx[training_size:, :]
 testing_Y = yy[training_size:, :]
 
-
 ##### end data
 
 params_options = {
-    "seed": [42],
-    "lr": [0.001, 0.0001, 0.01, 0.0005, 0.005],
-    "dropout": [0., 0.1, 0.2, 0.25, 0.05],
-    "hidden_sizes": [[20, 50, 20], [30, 40, 30], [10, 10, 10], [20, 80, 20], [10, 30, 20]]
+    "seed": [42, 124, 300],
+    "lr": [0.01, 0.001, 0.0001],
+    "dropout": [0., 0.05, 0.1, 0.25],
+    "hidden_sizes": [[2,4,2], [4, 8, 4], [8, 16, 8], [8, 32, 8], [16, 64, 32], [64, 128, 64]]
 }
 
 hyper_params = parameter_product(params_options)
+
 
 def config_architecture(params):
     # config of the architecture:
@@ -78,7 +76,7 @@ def config_architecture(params):
     activation_functions = [torch.tanh, torch.tanh, torch.relu]
     dropout = params["dropout"]
     epochs = 7500
-    batch_size = 200
+    batch_size = 1000
     optimiser = torch.optim.Adam
     criterion = nn.MSELoss(reduction='sum')
     dict_optimiser = {"lr": params["lr"], "weight_decay": 0.0000001}
@@ -96,9 +94,8 @@ def config_architecture(params):
     return param_training, Class_Parametrized_NN
 
 
-
 def generate_estims_history():
-    for i, params in enumerate(hyper_params):
+    for i, params in enumerate(tqdm(hyper_params)):
         # set seed for pytorch.
         set_seeds(params["seed"])
 
@@ -109,29 +106,35 @@ def generate_estims_history():
                                                   Class_Parametrized_NN,
                                                   param_train=param_training,
                                                   early_stoppers=early_stoppers,
-                                                  nb_split=9,
+                                                  nb_split=1,
                                                   shuffle_kfold=True,
-                                                  percent_val_for_1_fold=10,
-                                                  silent=False,
+                                                  percent_val_for_1_fold=20,
+                                                  silent=True,
                                                   hyper_param=params)
 
         estimator_history.to_json(path=f"sin_estim_history/estim_{i}.json")
 
+
 NEW_DATASET = False
 
 if __name__ == '__main__':
-
     if NEW_DATASET:
         generate_estims_history()
         estim = Estim_hyper_param.from_folder(path="sin_estim_history", metric_name="loss_validation")
+        # TODO REMOVE THE ESTIM_i.json !
         estim.to_csv("test_estim_hyper_param.csv")
-    else:
+    if not NEW_DATASET:
         estim = Estim_hyper_param.from_csv("test_estim_hyper_param.csv")
 
-    plot_hist_estim = Distplot_hyper_param(estim)
-    plot_hist_estim.hist(column_name_draw='loss_validation', separators_plot=['dropout'], hue='lr',
-                         palette='PuOr', bins=5,
-                         binrange=None, stat='count', multiple="layer", kde=True, path_save_plot=None)
+        histplot_hyperparam = Distplot_hyper_param(estim)
+        histplot_hyperparam.hist(column_name_draw='loss_validation', separators_plot=None, hue='hidden_sizes',
+                                 palette='RdYlBu', bins=20,
+                                 binrange=None, stat='count', multiple="stack", kde=False, path_save_plot=None)
 
-    APlot.show_plot()
+        histplot_hyperparam.hist(column_name_draw='loss_validation', separators_plot=None, hue='seed',
+                                 palette='RdYlBu', bins=20,
+                                 binrange=None, stat='count', multiple="dodge", kde=True, path_save_plot=None)
 
+        APlot.show_plot()
+
+        # TODO 06/07/2021 nie_k:  how could i multiply all loses by 1000?
